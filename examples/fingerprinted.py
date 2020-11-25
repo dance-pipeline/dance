43b04927bb8da1afc5afa4c5417f94a2d7a5e9a7
iBessonob <54378185+iBessonob@users.noreply.github.com>
2020-11-25 09:55:15 -0800

    Add files via upload

---
+++ examples/fingerprinted.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt 
+import math
+import argparse
+from collections import defaultdict
+from openeye import oechem
+
+def makeHistogram():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--smiles_database",
+                        type=str,
+                        required=True)
+    args = parser.parse_args()
+
+    ifs = oechem.oemolistream(args.smiles_database)
+    
+    histogram = []
+    
+    for molecule in ifs.GetOEMols():
+        smiles_mol = oechem.OEMolToSmiles(molecule)
+        mol = oechem.OEMol()
+        oechem.OESmilesToMol(mol, smiles_mol)
+        length = len([atom for atom in mol.GetAtoms()])
+        histogram.append(length)
+    plt.hist(histogram, density=True, bins=40)
+    plt.ylabel("Frequency")
+    plt.xlabel("Atom Count")
+    plt.title("Atom Frequency Probability of Fingerprinted Molecules")
+    plt.show()
+
+if __name__ == "__main__":
+    makeHistogram()
