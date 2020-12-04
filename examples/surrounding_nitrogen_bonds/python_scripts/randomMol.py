efce628c66163582f8b80490782285cd8a8eb5a1
iBessonob <bessonov3145@gmail.com>
2020-12-03 16:24:47 -0800

    Updated NN scripts

---
+++ examples/surrounding_nitrogen_bonds/python_scripts/randomMol.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt 
+import math
+import argparse
+from collections import defaultdict
+from openeye import oechem, oeomega
+
+def makeHistogram():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--smiles_database",
+                        type=str,
+                        required=True)
+    args = parser.parse_args()
+
+    ifs = oechem.oemolistream(args.smiles_database)
+    ofs = oechem.oemolostream("randDbFailedMols.smi") #keeps track of failed molecules
+
+    histogram = []
+    
+    for molecule in ifs.GetOEMols():
+        mol, status = smiles2oemol(oechem.OEMolToSmiles(molecule))
+        if (status):
+            length = len([atom for atom in mol.GetAtoms()])
+            histogram.append(length)
+        else:
+            oechem.OEWriteMolecule(ofs, mol)
+
+    plt.hist(histogram, color="red", density=False, bins=40)
+    plt.ylabel("# of Molecules", fontsize=16)
+    plt.xlabel("Atom Count", fontsize=16)
+    plt.title("Randomly Selected Molecules: N-N Bonds", fontsize=20)
+    plt.show()
+
+def smiles2oemol(smiles):
+    mol = oechem.OEMol()
+    oechem.OESmilesToMol(mol, smiles)
+
+    omega = oeomega.OEOmega()
+
+    omega.SetMaxConfs(1)
+    omega.SetIncludeInput(True)
+    omega.SetCanonOrder(True)
+    omega.SetSampleHydrogens(True)
+    omega.SetStrictStereo(True)
+    omega.SetStrictAtomTypes(True)
+    omega.SetIncludeInput(False)
+    status = omega(mol)
+    return (mol, status)
+
+if __name__ == "__main__":
+    makeHistogram()
